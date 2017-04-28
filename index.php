3b95a0ccc728f5c2cc9758503cca37b4c76a3130
Julia Yang <jyang587@gmail.com>
2017-04-28 13:54:24 -0400

    structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width">
+	<title>INFO2300 Final</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Raleway:200,400,500" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Raleway+Dots" rel="stylesheet">
+</head>
+
+<body>
+	<?php include "nav.html" ?>
+</body>
+
