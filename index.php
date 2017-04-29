baaf5b0f1d33d6a127d5f046d024e07b3666fbcb
Julia Yang <jyang587@gmail.com>
2017-04-29 12:17:41 -0400

    nav bar and logo

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
 	<title>INFO2300 Final</title>
 	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
 	<link href="https://fonts.googleapis.com/css?family=Raleway:200,400,500" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Raleway+Dots" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700" rel="stylesheet">
 </head>
 
 <body>
 	<?php include "nav.html" ?>
+	<img src="img/cme.png" id="logo" alt="CME Logo" width="15%">
+	<div class="greyContainer"><a href="index.php">Cornell Media & Entertainment</a></div>
 </body>
 
