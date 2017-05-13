c4b5acc51203c05779c2df462ebb77ea3a161f60
mlelien <emily.c.lien@gmail.com>
2017-05-13 00:13:38 -0400

    eboard

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,7 @@
 <?php 
 
 define('DB_HOST', 'localhost');
-
-define('DB_USER', 'jwy37sp17');    
- 
-define('DB_PASSWORD', 'info2300'); 
-
+define('DB_USER', 'ecl86sp17');    
+define('DB_PASSWORD', 'blurryface'); 
 define('DB_NAME', 'info230_SP17_fp_kje');    
 ?>
