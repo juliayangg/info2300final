e30e7b0a9e0682c3213cfb426cde11d1a9923657
coco9zhang <zhangke1925@gmail.com>
2017-05-13 16:09:54 -0400

    more styling on index page

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
         <title>Home | Cornell Media and Entertainment</title>
         <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+
         <?php 
             require_once "includes/functions.php";
             add_versioned_file( 'scripts/jquery-1.11.3.min.js', 'JavaScript' );
@@ -36,7 +38,7 @@
                 <div>
                     <img data-u="image" src="img/banner1.jpg" />
                     <div style="position:absolute;top:30px;left:30px;width:480px;height:120px;z-index:0;font-size:50px;color:#ffffff;line-height:60px;font-family: 'Raleway', sans-serif;">Cornell Media &amp; Entertainment</div>
-                    <div style="position:absolute;top:300px;left:30px;width:480px;height:120px;z-index:0;font-size:30px;color:#ffffff;line-height:38px;font-family: 'Montserrat', sans-serif;">Come and Join Us to Explore the Most Fun at Cornell</div>
+                    <div style="position:absolute;top:300px;left:30px;width:720px;height:120px;z-index:0;font-size:30px;color:#ffffff;line-height:38px;font-family: 'Montserrat', sans-serif;">CME is the first student-led multi-purpose organization on campus. Come and join us for the most fun at Cornell! </div>
                 </div>
                 <div>
                     <img data-u="image" src="img/banner2.jpg" />
@@ -58,32 +60,18 @@
         
         <div class="text">
             <h2>Important Notes:</h2>
-            <p>Hello INFO2300 graders! Welcome to this page. There are a few thing we would like you to know before grading this website.</p>
-            <p>First of all, in reality login function can only be accessed by manually change the URL to ".../login.php" as requested by the client. For your grading purpose, we also provide a link here: <a href=login.php>Login here</a>.</p>
-            <p>Secondly, regarding to the design, if you go to members page, you might notice the webpage is a little bit unbalanced with all information on the left. This is because we don't have enough data to populate the database. Once we populate the database, photos will cover the entire page and the page will be balance.</p>
+            <p>Hello INFO2300 graders! Welcome to this page. </p>
+            <p>Please note that, in reality login function can only be accessed by manually change the URL to ".../login.php" as requested by the client. For your grading purpose, we also provide a link here: <a href=login.php style="text-decoration:underline;">Login here</a>.</p>
             <h2>About Us</h2>
-            <img src="img/promo1.jpg" alt="CME Picture">
-            <p>Cornell Media &amp; Entertainment is the first student-led multi-purpose organization on campus.</p>
-            <p>The club aims to both support hands-on experience and exposure to Media and Entertainment Industry. We provide opportunities for event-planning and student-organized entertainment activities on campus.We also facilitate student interest in the Entertainment industry through career assistance, networking,and extensive support.</p>
+            <img src="img/all.jpg" alt="CME Picture">
+            <p style="text-align:left;">Cornell Media &amp; Entertainment is the first student-led multi-purpose organization on campus. It currently has around 20 officers, and are welcoming more new members to join!</p>
+            <p style="text-align:left;">The club aims to both support hands-on experience and exposure to Media and Entertainment Industry. We provide opportunities for event-planning and student-organized entertainment activities on campus. We also facilitate student interest in the Entertainment industry through career assistance, networking,and extensive support.</p>
+            <p style="text-align:left;">It was first founded in 2014 by several sophomores who were seeking for entertainment industry opportunities on campus. As all members of the team are Asians, they decided to found a club to promote Asian culture through running entertainment TV shows and micro films.</p>
         </div>
         
-        <div class="text2">
-            <h2>History</h2>
-            <img src="img/promo2.jpg" alt="CME Picture">
-            <p>Cornell Media &amp; Entertainment is first founded in 2014 by a group of sophomores. They were seeking for entertainment industry opportunities on campus but could not find any.</p>
-            <p>As all members of the team are Asians, they decided to found a club to promote Asian culture through running entertainment TV shows and micro films. Later on, CME developed strategic alliance with several organizations such CSSA (Chinese Students and Scholars Association) to increase its coverage on Cornell campus.</p>
+        <div class="welcome">
+            <h2>Welcome to Cornell Media &amp; Entertainment! </h2>
+            <h2>We appreciate your interests and look forward to seeing you at our events!</h2>
         </div>
-        
-        <!-- Psuedocode:
-            Instead of hardcoding all pictures, in this section we will display pictures from the database
-
-            Step 1: $result = $mysqli -> query('SELECT * FROM photos')
-            
-            Step 2: print out all file path in the format of <img class="picslides" src=file_path style="width:100%">
-            
-            Step 3: import the javascript
-            
-            Step 4: additional styling may be needed in case of weird size of pictures, but we'll leave it alone now. 
-            -->
     </body>
 </html>
